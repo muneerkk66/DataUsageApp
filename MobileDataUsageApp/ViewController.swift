b73afaf342d81e64d1671cad14a295393a17e303
Muneer KK <muneerkk66@gmail.com>
2020-04-03 23:52:10 +0530

    Initial Commit

---
+++ MobileDataUsageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileDataUsageApp
+//
+//  Created by x218507 on 03/04/20.
+//  Copyright © 2020 Muneer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MobileDataUsageAppTests/MobileDataUsageAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  MobileDataUsageAppTests.swift
+//  MobileDataUsageAppTests
+//
+//  Created by x218507 on 03/04/20.
+//  Copyright © 2020 Muneer. All rights reserved.
+//
+
+import XCTest
+@testable import MobileDataUsageApp
+
+class MobileDataUsageAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
